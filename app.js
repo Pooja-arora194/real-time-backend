64524e3ed9da3c9341408c932d141df0903e555d
Navjot Singh <navjot.seraphic@gmail.com>
2026-04-29 21:04:37 +0530

    Save

--- app.js
+++ app.js
@@ -12,7 +12,13 @@ const io = new Server(server, { cors: { origin: "*" } });
 app.set("io", io);
 connectDB();
 socketInit(io);
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true
+}));
 app.use(express.json());
 
 app.get("/", (req, res) => {
